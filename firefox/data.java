9bc76d982b26e9654b9d22eff0bdddbce374b9b5
MUTHURAMANA111 <muthur111@gmail.com>
2024-12-07 20:56:12 +0530

    Add files via upload
    day 1

---
+++ firefox/data.java
@@ -0,0 +1,22 @@
+package day1.firefox;
+
+public class data {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version=91.0f;
+		 String developer="Google";
+			boolean isBeta= false;
+		    int releaseYear=2008;
+		    char shortcutKey='C';
+		    System.out.println("chrome details:");
+		    System.out.println("version is:"+version+";\n"
+		    		+"Developer is:"+developer+";\n"
+		    		+"Beta is:"+isBeta+";\n"
+		    		+"Release Year is:"+releaseYear+";\n"
+		    		+"Short cut Key is:"+shortcutKey+";\n"
+		    	);
+
+	}
+
+}
---
+++ firefox/firefox.java
@@ -0,0 +1,21 @@
+package day1.firefox;
+
+public class firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion=100.2f;
+		String browserName="firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserLogo='f';
+		System.out.println("BrowserVersion is:"+browserVersion+";\n "
+				+"BrowserName is:" +browserName+";\n "
+				   + "Is the Browser Visible:"+isVisible+";\n "
+				       +"The Browser Released At:"+releaseYear+";\n"
+				              + "The Browser Logo:"+browserLogo+";"
+				              
+                );
+	}
+
+}
